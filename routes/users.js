4abd27fcf04fbaf40ff52ebd021a91fc716153db
coden-arfat <52942762+coden-arfat@users.noreply.github.com>
2022-05-01 21:38:08 +0600

    add user and get user program added
    Signed-off-by: coden-arfat <52942762+coden-arfat@users.noreply.github.com>

--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,59 @@ const express = require('express');
 const router = express.Router();
 const usersSchema=require('../models/user_model')
 
+// const data = {
+//   "name":"yasin arfat",
+//   "email":"arfat@example.com",
+//   "age":18
+// }
+// const addUser =new usersSchema(data)
+
+// addUser.save()
 // get all data 
 
-router.get('/', function(req, res, next) {
-  const data ={
-    'name ': ' yasin arfat',
-    'email ': 'xyz@email.com',
-    'date': Date.now,
-    'age ': 18,
-  }
-  res.json(data)
+router.get('/', async (req, res)=> {
+  
+
+    const users = await usersSchema.find()
+    return res.json(users)
+  
+
 }); 
 
 // get data by id
 
-router.get('/users/:id',function(req,res,next){
+router.get('/users/:id',function(req,res){
   
   res.send(req.params.id)
 })
+ 
+// posting data 
+
+router.post('/post/',async(req,res)=>{
+
+  console.log(  "name :",req.body.name,
+  "email:", req.body.email,
+  "age:", req.body.age)
+
+
+  const newUser = new usersSchema({
+  "name" : req.body.name,
+    "email": req.body.email,
+    "age" : req.body.age
+  })
+ try{
+  await newUser.save()
+    return res.status('201').json(newUser)
+  }
+  catch(err){
+  res.status('400').json({'message':err.message})
+  }
+
+
+
+})
+
+
 
 // update data by id 
 
