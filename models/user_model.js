90a6016f19e50b6f220c7372f11795d432373749
coden-arfat <52942762+coden-arfat@users.noreply.github.com>
2022-05-01 17:20:12 +0600

    created model using mongoose
    Signed-off-by: coden-arfat <52942762+coden-arfat@users.noreply.github.com>

--- models/user_model.js
+++ models/user_model.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose')
 
+const usersSchema = mongoose.Schema({
+  name :{
+    type : String,
+    required: ture
+  },
+  email :{
+    type:String,
+    required: ture
+  },
+  age:{
+    type : Number,
+    required: ture
+  },
+  date:{
+    type: Date,
+    required: ture,
+    default: Date.now
+    
+  }
+})
+
+module.exports= mongoose.model("UsersSchema",usersSchema)
