1ac77fef3eee738ca087ac7a20ed3bc7c0db3b0a
coden-arfat <coden-arfat>
2022-05-01 15:02:02 +0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   // res.render('index', { title: ' Hello Express' });
+//   res.send('hello world')
+// });
+
+router.get('/',function(req,res,next){
+
+  // res.send('hello i am learning express')
+  res.json({'hello':'world'})
+})
+
+module.exports = router;
