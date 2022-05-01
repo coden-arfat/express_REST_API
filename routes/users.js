88dc8bc8df854fc7c04b1013d56ab6b6ffc34f51
coden-arfat <52942762+coden-arfat@users.noreply.github.com>
2022-05-01 17:01:47 +0600

    created router
    Signed-off-by: coden-arfat <52942762+coden-arfat@users.noreply.github.com>

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. *
+// get all data 
 
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const data ={
+    'name ': ' yasin arfat',
+    'email ': 'xyz@email.com',
+    'date': Date.now,
+    'age ': 18,
+  }
+  res.json(data)
 }); 
+
+// get data by id
+
+router.get('/users/:id',function(req,res,next){
+  
+  res.send(req.params.id)
+})
+
+// update data by id 
+
+router.patch('/users/:id',function(req,res,next){
+  res.send(req.params.id)
+})
+
+// delete data by id 
+
+router.delete('/users/:id',function(req,res,next){
+  res.send(req.params,id)
+})
+
+// export router 
 
 module.exports = router;
